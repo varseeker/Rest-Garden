dd2ebc35cc7671cd92220ca224f36b4094c9f4b0
Ahsan <ibadahsan601@gmail.com>
2021-12-07 11:29:55 +0700

    [ASN][RG-032-BE] Upload Image and Get Image

---
+++ src/main/java/com/enigma/restgarden/ResourcesConfiguration.java
@@ -0,0 +1,19 @@
+package com.enigma.restgarden;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class ResourcesConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/picture/**")
+                .addResourceLocations("file:/home/dell/Academy/Pascal-7/RestGarden/upload-img/");
+
+    }
+}
---
+++ src/main/java/com/enigma/restgarden/service/StorageService.java
@@ -0,0 +1,26 @@
+package com.enigma.restgarden.service;
+
+import com.enigma.restgarden.service.user.UserServiceDbImpl;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class StorageService {
+
+
+    public void saveFileTo(MultipartFile multipartFile, String id) throws IOException {
+
+        String extension = FilenameUtils.getExtension(multipartFile.getOriginalFilename());
+//        File makeDir = new File("/home/dell/Academy/Pascal-7/RestGarden/upload-img/"+id);
+//        makeDir.mkdir();
+        String destination = "/home/dell/Academy/Pascal-7/RestGarden/upload-img/"+id+"."+extension;
+
+        File fileTemp = new File(destination);
+        multipartFile.transferTo(fileTemp);
+    }
+}
